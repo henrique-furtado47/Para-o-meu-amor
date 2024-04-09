4e2338828be9863d7f9fab96b817f502b4838bc6
henrique-furtado47 <henrique.furtadoifc@gmail.com>
2024-04-08 21:56:36 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var nome = prompt("Qual é o nome da pessoa mais incrível do mundo?");
+if (
+  nome == "Gabi" ||
+  nome == "gabi" ||
+  nome == "Gabrieli" ||
+  nome == "gabrieli" ||
+  nome == "GABRIELI" ||
+  nome == "GABI" ||
+  nome == "Bibi" ||
+  nome == "BIBI" ||
+  nome == "bibi"
+) {
+  alert(
+    "Você acertou! A pessoa mais incrível do mundo é o amor da minha vida!"
+  );
+} else {
+  alert("Você errou! A pessoa mais incrível do mundo é a minha gatinha Bibi!");
+}
